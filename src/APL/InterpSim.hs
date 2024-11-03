98c774ec7127dac638592eb887f80d106b0653b9
christianjensen2903 <cmj290300@gmail.com>
2024-11-03 13:29:34 +0100

    ✨ Simulated interpreter

--- src/APL/InterpSim.hs
+++ src/APL/InterpSim.hs
@@ -32,7 +32,37 @@ stateInitial = []
 --
 -- * Otherwise evaluate both one step.
 step :: Env -> State -> EvalM a -> (EvalM a, State)
-step = undefined
+step _ s (Pure x) = (Pure x, s)
+step r s (Free (ReadOp k)) = (k r, s)
+step _ s (Free (ErrorOp e)) = (Free (ErrorOp e), s)
+step _ s (Free (StepOp m)) = (m, s)
+step _ s (Free (KvGetOp key k)) =
+  case lookup key s of
+    Just val -> (k val, s)
+    Nothing -> (Free (KvGetOp key k), s)
+step _ s (Free (KvPutOp key val m')) =
+  (m', (key, val) : filter (\(k, _) -> k /= key) s)
+step r s (Free (BothOfOp m1 m2 k)) =
+  case (m1, m2) of
+    (Pure v1, Pure v2) -> (k (ValTuple [v1, v2]), s)
+    (Free (ErrorOp e), _) -> (Free (ErrorOp e), s)
+    (_, Free (ErrorOp e)) -> (Free (ErrorOp e), s)
+    _ -> let (m1', s') = step r s m1
+             (m2', s'') = step r s' m2
+         in (Free (BothOfOp m1' m2' k), s'')
+step r s (Free (OneOfOp m1 m2 k)) =
+  case (m1, m2) of
+    (Pure v1, _) -> (k v1, s)
+    (_, Pure v2) -> (k v2, s)
+    (Free (ErrorOp e1), Free (ErrorOp _)) -> (Free (ErrorOp e1), s)
+    _ -> let (m1', s') = step r s m1
+             (m2', s'') = step r s' m2
+         in (Free (OneOfOp m1' m2' k), s'')
 
 runEval :: EvalM a -> Either Error a
-runEval = undefined
+runEval = go envEmpty stateInitial
+  where
+    go r s comp = case step r s comp of
+      (Pure x, _) -> Right x
+      (Free (ErrorOp e), _) -> Left e
+      (m', s') -> go r s' m'
--- src/APL/InterpSim_Tests.hs
+++ src/APL/InterpSim_Tests.hs
@@ -24,4 +24,64 @@ tests :: TestTree
 tests =
   testGroup
     "Simulated concurrent interpreter"
-    []
+    [ testGroup
+        "Basic operations"
+        [ evalTest "Simple arithmetic"
+            (Add (CstInt 2) (CstInt 3))
+            (ValInt 5)
+        ],
+      testGroup
+        "BothOf operations"
+        [ evalTest "Basic both"
+            (BothOf (CstInt 1) (CstInt 2))
+            (ValTuple [ValInt 1, ValInt 2]),
+          evalTest "Nested both"
+            (BothOf
+              (Add (CstInt 1) (CstInt 2))
+              (Mul (CstInt 3) (CstInt 4)))
+            (ValTuple [ValInt 3, ValInt 12]),
+          evalTestFail "Error propagation"
+            (BothOf
+              (Div (CstInt 1) (CstInt 0))
+              (CstInt 2))
+        ],
+      testGroup
+        "OneOf operations"
+        [ evalTest "First succeeds"
+            (OneOf (CstInt 1) (CstInt 2))
+            (ValInt 1),
+          evalTest "For loop the fastest returns first"
+            (OneOf
+              (ForLoop ("x", CstInt 2) ("i", CstInt 5) (Var "x"))
+              (ForLoop ("x", CstInt 1) ("i", CstInt 2) (Var "x")))
+            (ValInt 1),
+          evalTest "Second succeeds when first fails"
+            (OneOf
+              (Div (CstInt 1) (CstInt 0))
+              (CstInt 2))
+            (ValInt 2),
+          evalTestFail "Both fail"
+            (OneOf
+              (Div (CstInt 1) (CstInt 0))
+              (Div (CstInt 2) (CstInt 0))),
+          evalTest "Infinite loop"
+            (OneOf (WhileLoop ("x", CstInt 2) (CstBool True) (Var "x"))
+              (CstInt 2))
+            (ValInt 2)
+        ],
+      testGroup
+        "Key-Value operations"
+        [ evalTest "Put and Get"
+            (Let "x" 
+              (KvPut (CstInt 1) (CstInt 42))
+              (KvGet (CstInt 1)))
+            (ValInt 42),
+          evalTest "Get waits for Put"
+            (Let "result"
+              (BothOf
+                (KvGet (CstInt 42))
+                (KvPut (CstInt 42) (CstInt 123)))
+              (Project (Var "result") 0))
+            (ValInt 123)
+        ]
+    ]
